127ca4bc2d06015cfcada0963a44dac09434d787
yashikagupta-ai <yashikagupta561@gmail.com>
2026-06-12 12:21:12 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://todo-backend-pki8.onrender.com";
+const API_URL = "https://todo-backend-pki8.onrender.com/todos";
 async function loadTodos() {
   const res = await fetch(API_URL);
   const todos = await res.json();
